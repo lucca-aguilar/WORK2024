b413f7edeb6babde44d9d4e308d09eecd9dcc01d
luccaaguilar <sophia.santos.aguilar@gmail.com>
2024-10-26 16:00:46 -0300

    Atualizações - 26/10
    Adiciona novos componentes na main, muda alguns pinos e adiciona testes comentados.

--- src/main.cpp
+++ src/main.cpp
@@ -1,195 +1,136 @@
 #include <AccelStepper.h>
-#include <UltrasonicSensor.h>
-#include <InfraredSensor.h>
-#include <ColorSensor.h>
-#include <Servo.h>
 #include <Arduino.h>
+#include <Bumper.h>
+#include <ColorSensor.h>
+#include <InfraredSensor.h>
+#include <LED.h>
+#include <MotorDC.h>
+#include <Robot.h>
+#include <Servo.h>
 #include <SoftwareSerial.h>
+#include <UltrasonicSensor.h>
+
+// declaracao de componentes
 
 // motores de passo
-AccelStepper motor1(AccelStepper::DRIVER, 50, 52);
+AccelStepper motor1(AccelStepper::DRIVER, 52, 50);
 AccelStepper motor2(AccelStepper::DRIVER, 53, 51);
-AccelStepper motor3(AccelStepper::DRIVER, 49, 47);
+AccelStepper motor3(AccelStepper::DRIVER, 41, 39);
 AccelStepper motor4(AccelStepper::DRIVER, 45, 43);
 
 // sensores ultrassônicos
-UltrasonicSensor usSensorFront(33, 31);
-UltrasonicSensor usSensorRight(22, 24);
+UltrasonicSensor usSensorFront(21, 20);
+UltrasonicSensor usSensorRight(16, 17);
 UltrasonicSensor usSensorLeft(37, 35);
 
 // sensores infravermelho
-InfraredSensor irSensorLFL(14, 0);  //lineFollowerLeft
+InfraredSensor irSensorLFL(5, 0);  //lineFollowerLeft
 InfraredSensor irSensorLFC(7, 0);  //lineFollowerCenter
 InfraredSensor irSensorLFR(8, 0);  //lineFollowerRight
 
-InfraredSensor irSensorTableHeight1(13, 0); //Top sensor
+InfraredSensor irSensorTableHeight1(6, 0); //Top sensor
 InfraredSensor irSensorTableHeight2(10 ,0);
 InfraredSensor irSensorTableHeight3(9 ,0);
 InfraredSensor irSensorTableHeight4(11, 0); //Bottom sensor
 
-
 // sensor de cor da garra
 ColorSensor clawSensor(30, 28, 24, 26, 22);
 
-// motor servo da garra
-Servo clawMotor();
+// servo motor da garra
+Servo clawServo;
 
-void runForward() {
-    motor2.move(-10000);
-    motor3.move(-10000);
+// leds
+LED blueLed(0);
+LED redLed(0);
 
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-    motor2.run();
-    motor3.run();
-  }
-};
+// raspberry
+SoftwareSerial raspy(0, 0);
 
-void runBackward() {
-    motor2.move(10000);
-    motor3.move(10000);
-
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-    motor2.run();
-    motor3.run();
-  }
-}
-
-void moveForward(int steps) {
-    motor2.move(-steps);
-    motor3.move(-steps);
-
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-    motor2.run();
-    motor3.run();
-  }
-};
-
-void moveBackward(int steps) {
-    motor2.move(steps);
-    motor3.move(steps);
-
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-        motor2.run();
-        motor3.run();
-    }
-};
-
-void moveLeft(int steps) {
-    motor1.move(steps);
-    motor4.move(steps);
-
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-        motor1.run();
-        motor4.run();
-    }
-};
-
-void moveRight(int steps) {
-    motor1.move(-steps);
-    motor4.move(-steps);
-
-    while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-        motor1.run();
-        motor4.run();
-    }
-};
-
-void rotateClockwise(int steps) {
-  motor1.move(steps);
-  motor2.move(steps);
-  motor3.move(-steps);
-  motor4.move(-steps);
-
-  while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-    motor1.run();
-    motor2.run();
-    motor3.run();
-    motor4.run();
-  }
-};
-
-void rotateAntiClockwise(int steps) {
-  motor1.move(-steps);
-  motor2.move(-steps);
-  motor3.move(steps);
-  motor4.move(steps);
-
-  while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0 || motor3.distanceToGo() != 0 || motor4.distanceToGo() != 0) {
-    motor1.run();
-    motor2.run();
-    motor3.run();
-    motor4.run();
-  }
-};
+// bumper
+Bumper clawBumper(0);
 
 void setup() {
+    // configuracao de porta serial
     Serial.begin(9600);
 
+    // configuracao comunicacao raspy (rx-tx)
+    raspy.begin(115200);
+    
+    // pino do servo motor 
+    clawServo.attach(11);
+
+    // configuração de velocidade e aceleração dos motores de passo
     motor1.setMaxSpeed(200);  // Velocidade máxima em passos/segundo
-    motor1.setAcceleration(200);  // Aceleração em passos/segundo^2
+    motor1.setAcceleration(100);  // Aceleração em passos/segundo^2
     
     motor2.setMaxSpeed(200);
-    motor2.setAcceleration(200);
+    motor2.setAcceleration(100);
 
     motor3.setMaxSpeed(200);
-    motor3.setAcceleration(200);
+    motor3.setAcceleration(100);
 
     motor4.setMaxSpeed(200);
-    motor4.setAcceleration(200);
+    motor4.setAcceleration(100);
 
 }
 
 void loop() {
-  while(true) {
-    
-    
-    
-    int LFL = irSensorLFL.checkNearby();
-    int LFC = irSensorLFC.checkNearby();
-    int LFR = irSensorLFR.checkNearby();
-    int frontDistance = usSensorFront.getDistance();
-    moveForward(10);
-
-    if (frontDistance < 15) {
-      moveLeft(200);
-      moveForward(370);
-      while (true) {
-        int LFR = irSensorLFR.checkNearby();
-        moveRight(200);
-        break;
-        /*if (LFR == 1) {
-          break;
-        }*/
-      }
-    }
-
-    /*
-
-    if (LFL == 0 && LFC == 1 && LFR == 0) {
-      moveForward(15);
-    }
-
-    if (LFL == 1 && LFC == 0 && LFR == 0) {
-      moveForward(5);
-      rotateClockwise(10);
-    }
-
-    if (LFL == 0 && LFC == 0 && LFR == 1) {
-      moveForward(5);
-      rotateAntiClockwise(10);
-    }
-
-    if (LFL == 0 && LFC == 0 && LFR == 0) {
-      moveForward(10);
-    }
-
-    if (LFL == 1 && LFC == 1 && LFR == 1) {
-      moveBackward(490);
-    }
-    */
+  
   }
 
-  }
+// comentado - testes
 
+/*
 
+// testes com infravermelho
+   int distanceIR1 = irSensorTableHeight1.measureDistance();
+   int distanceIR2 = irSensorTableHeight2.measureDistance();
+   int distanceIR3 = irSensorTableHeight3.measureDistance();
+   int distanceIR4 = irSensorTableHeight4.measureDistance();
 
+   int distanceLFL = irSensorLFL.measureDistance();
+   int distanceLFC = irSensorLFC.measureDistance();
+   int distanceLFR = irSensorLFR.measureDistance();
+
+   Serial.print("Ir1 =");
+   Serial.println(distanceIR1);
+   Serial.print("Ir2 =");
+   Serial.println(distanceIR2);
+   Serial.print("Ir3 =");
+   Serial.println(distanceIR3);
+   Serial.print("Ir4 =");
+   Serial.println(distanceIR4);
+   Serial.println(".");
+
+   Serial.print("IrLFL =");
+   Serial.println(distanceLFL);
+   Serial.print("IrLFC =");
+   Serial.println(distanceLFC);
+   Serial.print("IrLFR =");
+   Serial.println(distanceLFR);
+   Serial.println(".");
+
+// testes com ultrassonicos
+   int distanceLeft = usSensorLeft.getDistance();
+   int distanceFront = usSensorFront.getDistance();
+   int distanceRight = usSensorRight.getDistance();
+
+   Serial.print("USL =");
+   Serial.println(distanceLeft);
+   Serial.print("USF =");
+   Serial.println(distanceFront);
+   Serial.print("USR =");
+   Serial.println(distanceRight);
+
+// teste servo
+  clawServo.write(80);
+  delay(2000);
+  clawServo.write(105);
+  delay(2000);
+
+// teste com motores de passo (movimentacao)
+  moveForward(100);
+  moveBackward(100);
+  moveRight(100);
+  moveLeft(100);
+*/
